16a0751e8ccc89e4e0fd4af716ae874ad0e6d562
lisa <lisa.ng8@hotmail.com>
2020-01-14 16:11:12 +1100

    Updates

---
+++ precache-manifest.8b21b5aafcfd5f3b84fffcb44ac354d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0177909dbaccb5a5e5a40a270e3b25b",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "8e2a718555a2c14ac2d6",
+    "url": "/random-quote-machine/static/css/main.93137d16.chunk.css"
+  },
+  {
+    "revision": "41a3b3229900df4f7dc5",
+    "url": "/random-quote-machine/static/js/2.db93165b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/random-quote-machine/static/js/2.db93165b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8e2a718555a2c14ac2d6",
+    "url": "/random-quote-machine/static/js/main.27de61fd.chunk.js"
+  },
+  {
+    "revision": "7685529411e0d4ec2671",
+    "url": "/random-quote-machine/static/js/runtime-main.b9bf67be.js"
+  }
+]);
